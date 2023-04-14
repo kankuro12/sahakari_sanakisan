edb06cbf0fb029d172a7da2ff9af405b6e1e9ef8
chhatraman shrestha <cms111000111@gmail.com>
2023-04-14 11:54:40 +0545

    data

---
+++ resources/views/front/layout/menu.blade.php
@@ -0,0 +1,49 @@
+<!-- Navbar Start -->
+<div class="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
+    <div class="top-bar row gx-0 align-items-center d-none d-lg-flex">
+    <div class="col-lg-6 px-5 text-start">
+        <small><i class="fa fa-map-marker-alt text-primary me-2"></i>sdfsad</small>
+        <small class="ms-4"><i class="fa fa-clock text-primary me-2"></i>asdfasdf</small>
+    </div>
+    <div class="col-lg-6 px-5 text-end">
+        <small><i class="fa fa-envelope text-primary me-2"></i>sadfasdf</small>
+        <small class="ms-4"><i class="fa fa-phone-alt text-primary me-2"></i>asdfasdf</small>
+    </div>
+</div>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
+        <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
+            <h1 class="display-5 text-primary m-0">Laravel</h1>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+
+                                                        <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">one</a>
+                        <div class="dropdown-menu border-light m-0">
+                                                            <a href="/SADFASDFS" class="dropdown-item">test</a>
+                                                    </div>
+                    </div>
+                                                </div>
+
+            <div class="d-none d-lg-flex ms-2">
+    <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="/lorem">
+        <small class="fab fa-facebook-f text-primary"></small>
+    </a>
+    <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="/twitter">
+        <small class="fab fa-twitter text-primary"></small>
+    </a>
+    <a class="btn btn-light btn-sm-square rounded-circle ms-3" href="/toutube">
+        <small class="fab fa-youtube text-primary"></small>
+    </a>
+</div>
+        </div>
+    </nav>
+</div>
+<!-- Navbar End -->
---
+++ resources/views/front/layout/top.blade.php
@@ -0,0 +1,12 @@
+<div class="top-bar row gx-0 align-items-center d-none d-lg-flex">
+    <div class="col-lg-6 px-5 text-start">
+        <small><i class="fa fa-map-marker-alt text-primary me-2"></i>sdfsad</small>
+        <small class="ms-4"><i class="fa fa-clock text-primary me-2"></i>asdfasdf</small>
+    </div>
+    <div class="col-lg-6 px-5 text-end">
+        <small><i class="fa fa-envelope text-primary me-2"></i>sadfasdf</small>
+        <small class="ms-4"><i class="fa fa-phone-alt text-primary me-2"></i>asdfasdf</small>
+    </div>
+</div>
+
+
