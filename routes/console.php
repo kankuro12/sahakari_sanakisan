883c983f172b89add40f651c0d70351bf170923e
chhatraman shrestha <cms111000111@gmail.com>
2024-10-23 16:24:24 +0545

    make admin password

--- routes/console.php
+++ routes/console.php
@@ -16,11 +16,41 @@ use Illuminate\Support\Facades\Artisan;
 | simple approach to interacting with each command's IO methods.
 |
 */
-
 Artisan::command('make:admin', function () {
-    $user=new User();
-    $user->name="Admin";
-    $user->email="admin@sabalskkss.com.np";
-    $user->password=bcrypt('dwZuXUUlOYdn1Vp8');
+    $name = $this->ask('What is the admin name?');
+    $email = $this->ask('What is the admin email?');
+    $password = $this->secret('What is the admin password?');
+
+    $user = new User();
+    $user->name = $name;
+    $user->email = $email;
+    $user->password = bcrypt($password);
     $user->save();
-})->purpose('Seeding downloads');
+
+    $this->info('Admin user created successfully.');
+})->purpose('Create an admin user');
+
+Artisan::command('change:password', function () {
+    $email = $this->ask('Enter the email of the user whose password you want to change:');
+    $user = User::where('email', $email)->first();
+
+    if (!$user) {
+        $this->error('User with this email does not exist.');
+        return;
+    }
+
+    $newPassword = $this->secret('Enter the new password:');
+    $confirmPassword = $this->secret('Confirm the new password:');
+
+    // Check if the new password matches the confirmation
+    if ($newPassword !== $confirmPassword) {
+        $this->error('New password and confirmation password do not match.');
+        return;
+    }
+
+    // Update the password
+    $user->password = bcrypt($newPassword);
+    $user->save();
+
+    $this->info('Password changed successfully.');
+})->purpose('Change the user password without verifying current password');
