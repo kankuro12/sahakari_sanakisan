ca52801eea11c7aebf396716a8d071a3d15da16e
chhatraman shrestha <cms111000111@gmail.com>
2024-10-27 16:37:41 +0545

    datatable added in the downloads

--- resources/views/front/pages/gallery/pdfgallery.blade.php
+++ resources/views/front/pages/gallery/pdfgallery.blade.php
@@ -10,7 +10,8 @@
 @section('meta')
 @endsection
 @section('css')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
 @endsection
 @section('title')
     - Downloads
@@ -19,10 +20,14 @@
     Downloads
 @endsection
 @section('pagecontent')
-    <div class="container p-2">
-        @includeIf('front.pages.partials.pdfgalleries')
-    </div>
+    @includeIf('front.pages.partials.pdfgalleries')
 @endsection
 @section('js')
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#pdfTable').DataTable();
+        });
+    </script>
 @endsection
