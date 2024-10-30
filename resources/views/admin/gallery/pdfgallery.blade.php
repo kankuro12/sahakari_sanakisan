90bda7e5dbf95c18949641b205e4029dda15493c
chhatraman shrestha <cms111000111@gmail.com>
2024-10-30 08:16:45 +0545

    pdf changed

--- resources/views/admin/gallery/pdfgallery.blade.php
+++ resources/views/admin/gallery/pdfgallery.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.layout.app')
 @section('css')
     <link rel="stylesheet" href="{{ asset('admin/plugins/drophify/css/dropify.min.css') }}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <style>
         .col-md-3 {
             margin-bottom: 10px;
@@ -59,54 +60,53 @@
                         </div>
                     </form>
                 </div>
-                @foreach ($pdfs as $pdf)
-                    <div class="col-md-4">
-                        <form class="shadow p-2" action="{{ route('admin.setting.gallery.pdf.edit', ['pdf' => $pdf->id]) }}"
-                            enctype="multipart/form-data" method="post">
-                            @csrf
-                            <div class="form-group">
-                                <label for="title">Title</label>
-                                <input type="text" name="title" value="{{ $pdf->title }}" id="title"
-                                    class="form-control" required>
-                            </div>
 
-                            <div class="form-group">
-                                <label for="date">Date</label>
-                                <input type="text" name="date" value="{{ $pdf->date }}" id="date" class="form-control calender" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="mode">Mode</label>
-                                <select name="mode" id="mode" class="form-control">
-                                    <option value="public" {{ $pdf->mode == 'public' ? 'selected' : '' }}>Public</option>
-                                    <option value="private" {{ $pdf->mode == 'private' ? 'selected' : '' }}>Private</option>
-                                </select>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="pdf">PDF</label>
-                                <input type="file" name="pdf" id="pdf" class="form-control photo"
-                                    accept="application/pdf" data-default-file="{{ asset($pdf->pdf) }}">
-                            </div>
-
-                            <div class="py-2 d-flex justify-content-between">
-                                <button class="btn btn-secondary">Update PDF</button>
-                                <a onclick="return prompt('Enter yes To Continue.')=='yes';"
-                                    href="{{ route('admin.setting.gallery.pdf.del', ['pdf' => $pdf->id]) }}"
-                                    class="btn btn-danger">Del</a>
-                            </div>
-                        </form>
-                    </div>
-                @endforeach
+                <div class="col-md-8">
+                    <table id="pdf-table" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Title</th>
+                                <th>Date</th>
+                                <th>Mode</th>
+                                <th>PDF</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($pdfs as $pdf)
+                                <tr id="pdf-{{ $pdf->id }}">
+                                    <td>{{ $pdf->title }}</td>
+                                    <td>{{ $pdf->date }}</td>
+                                    <td>{{ ($pdf->mode) }}</td>
+                                    <td>
+                                        <a href="{{ asset($pdf->pdf) }}" target="_blank">View Document</a>
+                                    </td>
+                                    <td>
+                                        {{-- <form class="d-inline" action="{{ route('admin.setting.gallery.pdf.edit', ['pdf' => $pdf->id]) }}" method="post" enctype="multipart/form-data">
+                                            @csrf
+                                            <button class="btn btn-secondary">Update</button>
+                                        </form> --}}
+                                        <a onclick="return prompt('Enter yes To Continue.')=='yes';"
+                                            href="{{ route('admin.setting.gallery.pdf.del', ['pdf' => $pdf->id]) }}"
+                                            class="btn btn-danger">Del</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 @endsection
+
 @section('script')
     <script src="{{ asset('admin/plugins/drophify/js/dropify.min.js') }}"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <script>
         $(document).ready(function() {
             $('.photo').dropify();
+            $('#pdf-table').DataTable();
         });
     </script>
     @include('admin.layout.datepicker');
--- resources/views/admin/gallery/pdftemplate.blade.php
+++ resources/views/admin/gallery/pdftemplate.blade.php
@@ -1,26 +1,19 @@
 <div class="container-xxl">
     <div class="container">
-        <table id="pdfTable" class="display" style="width:100%">
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Date</th>
-                    <th>Download</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($pdfgalleries as $item)
-                    <tr>
-                        <td>{{ $item->title }}</td>
-                        <td>{{ $item->date }}</td>
-                        <td>
-                            <a href="{{asset($item->pdf)}}" download>
-                                <i class="fa-solid fa-download"></i>
-                            </a>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="row" style="column-gap: 20px">
+            @foreach ($pdfgalleries as $item)
+                <div class="col-md-3 shadow p-4">
+                    <a href="{{asset($item->pdf)}}" download>
+                        <i class="fa-solid fa-download"></i>
+                    </a>
+                    <h4 class="name mt-2">
+                        {{ $item->title }}
+                    </h4>
+                    <p class="desc mb-2">
+                        {{ $item->date }}
+                    </p>
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
