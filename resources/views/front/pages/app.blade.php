93d03d269d6d618944eea3f4e4a465796e8ac0e9
komal shrestha <komalstha0000.com>
2025-03-05 16:00:17 +0545

    fix: adjust page header padding and improve CSS formatting for better readability

--- resources/views/front/pages/app.blade.php
+++ resources/views/front/pages/app.blade.php
@@ -4,7 +4,7 @@
 @endsection
 @section('content')
     <!-- Page Header Start -->
-    <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s"
+    <div class="container-fluid page-header wow fadeIn" data-wow-delay="0.1s"
         style="background-image: url('{{ asset('uploads/header.jpg') }}') !important">
         <div class="container">
             <h1 class="display-3 mb-4 animated slideInDown single-line">@yield('b-title')</h1>
