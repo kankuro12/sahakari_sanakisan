c9e851399ff7cf7e8b1e12b5f7fafdac0715ff58
komal shrestha <komalstha0000.com>
2025-03-06 17:51:31 +0545

    fix: update team pages to support Nepali translations and improve layout structure

--- resources/views/admin/setting/template/homefacts.blade.php
+++ resources/views/admin/setting/template/homefacts.blade.php
@@ -1,37 +1,19 @@
-   <!-- Facts Start -->
+<div _ngcontent-vjb-c94="" class="fact-container">
+    <div _ngcontent-vjb-c94="" class="container">
+        <div _ngcontent-vjb-c94="" class="row">
+            @for ($i = 1; $i <= 6; $i++)
+                <div _ngcontent-vjb-c94="" class="col-lg-4 col-md-6 col-sm-6 col-12">
+                    <div _ngcontent-vjb-c94="" class="profile-card">
+                        <div _ngcontent-vjb-c94="" class="icon text-white"><img _ngcontent-vjb-c94=""
+                                src="{{ asset($curdata['icon' . $i]) }}" alt="Members"></div>
+                        <div _ngcontent-vjb-c94="" class="profile-content">
+                            <h6 _ngcontent-vjb-c94="">{{ $curdata['title' . $i] }}</h6>
+                            <h5 _ngcontent-vjb-c94="" class="countUp">{{ $curdata['num' . $i] }}</h5>
+                        </div>
+                    </div>
+                </div>
+            @endfor
 
-   {{-- <div class="container-fluid facts my-5 py-5">
-       <div class="container py-5">
-           <div class="row g-5">
-               @for ($i = 1; $i <= 6; $i++)
-                   <div class="col-md-4 col-sm-6 col-lg-4 text-center wow fadeIn"
-                       data-wow-delay="{{ 0.1 + ($i - 1) * 0.2 }}s">
-                    <div class="single">
-
-                        <h1 data-toggle="counter-up"></h1>
-                        <span class="fs-5 text-white"></span>
-                    </div>   </div>
-               @endfor
-
-           </div>
-       </div>
-   </div> --}}
-   <div _ngcontent-vjb-c94="" class="fact-container">
-       <div _ngcontent-vjb-c94="" class="container">
-           <div _ngcontent-vjb-c94="" class="row">
-               @for ($i = 1; $i <= 6; $i++)
-                   <div _ngcontent-vjb-c94="" class="col-lg-4 col-md-6 col-sm-6 col-12">
-                       <div _ngcontent-vjb-c94="" class="profile-card">
-                           <div _ngcontent-vjb-c94="" class="icon text-white"><img _ngcontent-vjb-c94=""
-                                   src="{{ asset($curdata['icon' . $i]) }}" alt="Members"></div>
-                           <div _ngcontent-vjb-c94="" class="profile-content">
-                               <h6 _ngcontent-vjb-c94="">{{ $curdata['title' . $i] }}</h6>
-                               <h5 _ngcontent-vjb-c94="" class="countUp">{{ $curdata['num' . $i] }}</h5>
-                           </div>
-                       </div>
-                   </div>
-               @endfor
-
-           </div>
-       </div>
-   </div>
+        </div>
+    </div>
+</div>
--- resources/views/front/pages/team/type.blade.php
+++ resources/views/front/pages/team/type.blade.php
@@ -1,47 +1,44 @@
 @extends('front.pages.app')
 @section('b-items')
-<li class="breadcrumb-item active" aria-current="page">Members</li>
-
+    <li class="breadcrumb-item active" aria-current="page">समितिहरू</li>
 @endsection
 @section('pagecss')
     <style>
-       .team-holder{
+        .team-holder {}
 
-       }
-       .team-holder .team{
-        display: block;
-        color: black;
-         box-shadow: 0px 0px 10px 0px #355EFC54;border-radius:5px;overflow:hidden;
-       }
+        .team-holder .team {
+            display: block;
+            color: black;
+            box-shadow: 0px 0px 10px 0px #355EFC54;
+            border-radius: 5px;
+            overflow: hidden;
+        }
 
-       .team-holder .team .image{
-        background: #DEE1E6;
-       }
+        .team-holder .team .image {
+            background: #DEE1E6;
+        }
 
-       .team-holder .team .name{
-
-       }
+        .team-holder .team .name {}
 
 
-       @media(max-width:425px){
-            .team-holder{
+        @media(max-width:425px) {
+            .team-holder {
                 padding: 0px 5px !important;
             }
 
-            .team-holder .team{
+            .team-holder .team {
                 box-shadow: 0px 0px 5px 0px #355EFC54;
             }
-       }
-
+        }
     </style>
 @endsection
 @section('b-title')
-    Members
+    समितिहरू
 @endsection
 @section('pagecontent')
-<div class="container-xxl py-5">
-    <div class="container">
-        @include('front.pages.partials.team')
+    <div class="container-xxl py-5">
+        <div class="container">
+            @include('front.pages.partials.team')
+        </div>
     </div>
-</div>
 @endsection
