ed54cf4552a30b502fda545336966ae429bc3d55
komal shrestha <komalstha0000.com>
2025-03-17 13:01:22 +0545

    fix: adjust media query for logo size and enhance navbar structure with side panel

--- resources/views/admin/menu/template.blade.php
+++ resources/views/admin/menu/template.blade.php
@@ -4,36 +4,14 @@
         height: auto;
     }
 
-    @media (max-width: 425px) {
-        .h-logo {
-            max-width: 70px;
-        }
-
-        .h-logo img {
-            width: 100%;
-        }
-
-        .navbar-nav {
-            flex-direction: column;
-        }
-
-        .nav-item {
-            width: 100%;
-        }
-
-        .dropdown-menu {
-            position: static;
-            float: none;
-            width: 100%;
-            background-color: #077430;
-        }
-
-        .dropdown-item {
-            color: white;
+    @media (max-width: 725px) {
+        .mobile-view {
+            display: block;
         }
     }
 </style>
-<nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s" style="background-color:#077430 ">
+<nav class="navbar navbar-expand-lg navbar-light py-lg-0 px-lg-5 wow " data-wow-delay="0.1s"
+    style="background-color:#077430 ">
     <div class="collapse navbar-collapse" id="navbarCollapse" style="padding: 0px 31px">
         <div class="navbar-nav  p-lg-0">
             @foreach ($menus as $menu)
@@ -54,4 +32,38 @@
             @endforeach
         </div>
     </div>
+    <div class="mobile-view d-lg-none">
+        <div class="px-3" onclick="openPannel()">
+            <i class="fa fa-bars text-white"></i>
+        </div>
+        <div class="pannel px-4 py-2" id="pannel" style="display:none">
+            @foreach ($menus as $menu)
+                @if ($menu->is_header)
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link text-white dropdown-toggle"
+                            data-bs-toggle="dropdown">{{ $menu->name }}</a>
+                        <div class="dropdown-menu border-light m-0">
+                            @foreach ($menu->childs() as $child)
+                                <a href="{{ $child->link }}" class=""
+                                    class="dropdown-item">{{ $child->name }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                @else
+                    <a href="{{ $menu->link }}" class="nav-item nav-link text-white">{{ $menu->name }}</a>
+                @endif
+            @endforeach
+        </div>
+    </div>
+
 </nav>
+<script>
+    function openPannel() {
+        var pannel = $('#pannel');
+        if (pannel.is(':visible')) {
+            pannel.hide();
+        } else {
+            pannel.show();
+        }
+    }
+</script>
